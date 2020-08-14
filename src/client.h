9926606db637d73434c21d919e52eeb6b678a4f6
Patrick Steinhardt <ps@pks.im>
2020-08-14 17:30:33 +0200

    Update copyright notice
    Update the copyright notice to include 2020.

--- src/client.h
+++ src/client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Patrick Steinhardt, 2019
+ * Copyright (C) Patrick Steinhardt, 2019-2020
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
