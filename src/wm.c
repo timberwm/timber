3e8b2406567f87006e0685ec54bc98c31ee056bb
Patrick Steinhardt <ps@pks.im>
2026-03-28 16:34:42 +0100

    wm: drop wlr-data-control-v1 in favor of ext-data-control-v1
    Drop the deprecated wlr-data-control-v1 protocol in favor of the
    official ext-data-control-v1 protocol.

--- src/wm.c
+++ src/wm.c
@@ -36,9 +36,9 @@
 #include <wlr/types/wlr_color_representation_v1.h>
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_cursor.h>
-#include <wlr/types/wlr_data_control_v1.h>
 #include <wlr/types/wlr_data_device.h>
 #include <wlr/types/wlr_export_dmabuf_v1.h>
+#include <wlr/types/wlr_ext_data_control_v1.h>
 #include <wlr/types/wlr_ext_image_copy_capture_v1.h>
 #include <wlr/types/wlr_fractional_scale_v1.h>
 #include <wlr/types/wlr_gamma_control_v1.h>
@@ -2146,8 +2146,8 @@ int tmbr_wm(void)
 	    wlr_color_representation_manager_v1_create_with_renderer(server.display, 1, server.renderer) == NULL ||
 	    wlr_alpha_modifier_v1_create(server.display) == NULL ||
 	    wlr_data_device_manager_create(server.display) == NULL ||
-	    wlr_data_control_manager_v1_create(server.display) == NULL ||
 	    wlr_export_dmabuf_manager_v1_create(server.display) == NULL ||
+	    wlr_ext_data_control_manager_v1_create(server.display, 1) == NULL ||
 	    wlr_ext_image_copy_capture_manager_v1_create(server.display, 1) == NULL ||
 	    wlr_fractional_scale_manager_v1_create(server.display, 1) == NULL ||
 	    wlr_presentation_create(server.display, server.backend, 2) == NULL ||
